9fee44ccabfde483232caf9a6e18a3ba940c96d1
Rachit jain <rachitpersonal64772@gmail.com>
2025-06-02 14:20:22 +0000

    commit

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from PIL import Image
 
+st.title("🕺 AI Dance Step Detector")
+
+uploaded_file = st.file_uploader("Upload your dance pose image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.success("Image uploaded successfully!")
